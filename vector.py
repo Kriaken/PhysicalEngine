75642e725851321d4c3d4c9644e23206730a7c6e
Ivan Davydenko <davidenkoivan2005@gmail.com>
2024-08-04 16:59:26 +0200

    Vector class with supplementary functions created

---
+++ vector.py
@@ -0,0 +1,27 @@
+import math
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def X(self):
+        return self.x
+
+    def Y(self):
+        return self.y
+    
+    def vector_length(self):
+        return (self.X**2 + self.Y**2)**(1/2)
+
+def dot_product(a, b):
+    return Vector(a.X()*b.X(), a.Y()*b.Y())
+
+def angle_vectors(a, b):
+    return math.acos(dot_product(a, b)/(a.vector_length()*b.vector_length()))
+
+def cross_product(a, b):
+    phi = angle_vectors(a, b)
+
+    return a.vector_length()*b.vector_length()*math.sin(phi)
